3755836ed8d8f9f724044ea96e15166598c91861
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2018-11-29 13:22:32 +0000

    Fixed

--- ratios.py
+++ ratios.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #asks for the ratios for the cards you are farming and exalts
 exaltrat = int(input('Exalt ratio: '))
 cardrat = int(input('Card ratio: '))
@@ -8,13 +7,3 @@ with open('exaltrat.txt', 'w') as f:
 
 with open('cardrat.txt','w') as r:
     r.write(str(cardrat))
-=======
-exaltrat = int(input('Exalt ratio: '))
-cardrat = int(input('Card ratio: '))
-
-with open('exaltrat.txt', 'w') as f:
-    f.write(str(exaltrat))
-
-with open('cardrat.txt','w') as r:
-    r.write(str(cardrat))
->>>>>>> 482c17b7da01d681e3baaa759b52551c40d830f8
