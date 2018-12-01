abd4f7cabdbb6617a213919328b3814beb650c51
KRoses96 <45322919+KRoses96@users.noreply.github.com>
2018-12-01 02:04:29 +0000

    Delete ratios.py

--- ratios.py
+++
@@ -1,9 +0,0 @@
-#asks for the ratios for the cards you are farming and exalts
-exaltrat = int(input('Exalt ratio: '))
-cardrat = int(input('Card ratio: '))
-
-with open('exaltrat.txt', 'w') as f:
-    f.write(str(exaltrat))
-
-with open('cardrat.txt','w') as r:
-    r.write(str(cardrat))
